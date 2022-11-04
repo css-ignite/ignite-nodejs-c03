ddc668cdccd196c71ac5d4be33f43665d8abb71e
claudneysessa <claudneysartisessa@gmail.com>
2022-11-04 02:14:50 -0300

    Ajustando o server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
-import express from "express";
-
+import express, { NextFunction, Request, Response } from "express";
+import { AppError } from "./AppErros";
 import { router } from "./routes";
 
 import "./database";
@@ -10,6 +10,20 @@ const port = 3333;
 app.use(express.json());
 app.use("/v1", router);
 
+app.use(
+    (err: Error, request: Request, response: Response, next: NextFunction) => {
+        if (err instanceof AppError) {
+            return response.status(err.statusCode).json({
+                message: err.message,
+            });
+        }
+        return response.status(500).json({
+            status: "error",
+            message: `Internal server error - ${err.message}`,
+        });
+    }
+);
+
 app.listen(port, () => {
-    console.log("Server is running on por", port);
+    console.log("Server is running on port", port);
 });
