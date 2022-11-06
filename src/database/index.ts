e4b5cca0f8cc4ff2683031ae140673ef30e0635e
Claudney Sarti Sessa <claudneysartisessa@gmail.com>
2022-11-06 16:46:00 -0300

    Tentando resolver o problema de conexão

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,27 +3,29 @@
 // TENTATIVA 3
 // -----------------------------
 
-// import { createConnection, getConnectionOptions } from 'typeorm';
+import { createConnection, getConnectionOptions } from 'typeorm';
 
-// interface IOptions {
-//     host: string;
-// }
+interface IOptions {
+    host: string;
+}
 
-// getConnectionOptions().then(options => {
-//     const newOptions = options as IOptions;
-//     newOptions.host = 'ignite_rentx_database'; //Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
-//     createConnection({
-//         ...options,
-//     })
-//         .then(() => {
-//             console.log("createConnection - Connected to database!");
-//         })
-//         .catch((error) => {
-//             console.error("createConnection - Connected fail!", error);
-//         });
-// });
+getConnectionOptions().then(options => {
+    const newOptions = options as IOptions;
+    newOptions.host = 'ignite_rentx_database'; //Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
+    createConnection({
+        ...options,
+    })
+        .then(() => {
+            console.log("- - - - - - - - - - - - - - - - - - - - - - -");
+            console.log("Connected to database!");
+            console.log("- - - - - - - - - - - - - - - - - - - - - - -");
+        })
+        .catch((error) => {
+            console.error("Connected fail!", error);
+        });
+});
 
-// export { createConnection };
+export { createConnection };
 
 
 // -----------------------------
@@ -49,27 +51,27 @@
 // -----------------------------
 
 
-import { DataSource } from "typeorm";
+// import { DataSource } from "typeorm";
 
-const datasource = new DataSource({
-    type: "postgres",
-    host: "ignite_rentx_database",
-    port: 5432,
-    username: "docker",
-    password: "ignite",
-    database: "rentx",
-    synchronize: true,
-    logging: true,
-});
+// const datasource = new DataSource({
+//     type: "postgres",
+//     host: "ignite_rentx_database",
+//     port: 5432,
+//     username: "docker",
+//     password: "ignite",
+//     database: "rentx",
+//     synchronize: true,
+//     logging: true,
+// });
 
-const appDataSource = datasource.initialize()
-    .then(() => {
-        console.log("- - - - - - - - - - - - - - - - - - - - - - -");
-        console.log("Connected to database!");
-        console.log("- - - - - - - - - - - - - - - - - - - - - - -");
-    })
-    .catch((error) => {
-        console.error("Connected fail!", error);
-    });
+// const appDataSource = datasource.initialize()
+//     .then(() => {
+//         console.log("- - - - - - - - - - - - - - - - - - - - - - -");
+//         console.log("Connected to database!");
+//         console.log("- - - - - - - - - - - - - - - - - - - - - - -");
+//     })
+//     .catch((error) => {
+//         console.error("Connected fail!", error);
+//     });
 
-export { datasource };
+// export { datasource };
